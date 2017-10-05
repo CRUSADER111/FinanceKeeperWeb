3899a24a2ade4dad98173ddd53612d3904a22f4c
Alex Morrison <asmorrison@hotmail.co.uk>
2017-10-05 16:42:45 +0100

    Updated auth.php to connect to a specific database

--- auth.php
+++ auth.php
@@ -2,9 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "financedb";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
